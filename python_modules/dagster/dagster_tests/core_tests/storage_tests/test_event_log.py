0de467d5cfe3754531c8bb4829030597a9e52752
Max Gasner <max.gasner@gmail.com>
2020-01-29 13:37:32 -0800

    Safe concurrency for SQLite initdb
    Summary: This handles issues with concurrent nodes in Airflow.
    
    Test Plan: Unit
    
    Reviewers: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D1921

--- python_modules/dagster/dagster_tests/core_tests/storage_tests/test_event_log.py
+++ python_modules/dagster/dagster_tests/core_tests/storage_tests/test_event_log.py
@@ -237,7 +237,6 @@ def test_filesystem_event_log_storage_run_corrupted_bad_data():
             storage.get_logs_for_run('bar')
 
 
-@pytest.mark.skip  # https://github.com/dagster-io/dagster/issues/2098
 def test_concurrent_sqlite_event_log_connections():
     exceptions = multiprocessing.Queue()
 
@@ -255,7 +254,7 @@ def test_concurrent_sqlite_event_log_connections():
 
         ps = []
         for _ in range(5):
-            ps.append(multiprocessing.Process(target=cmd, args=(exceptions,)))
+            ps.append(multiprocessing.Process(target=cmd, args=(exceptions)))
         for p in ps:
             p.start()
 
